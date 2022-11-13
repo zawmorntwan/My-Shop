b223e0ddc9a06407749c0d0a3c9b8345981987cf
Zaw Min Tun <zawmintun.tp@gmail.com>
2022-11-13 15:01:17 +0630

    Update product.dart

--- lib/providers/product.dart
+++ lib/providers/product.dart
@@ -1,4 +1,9 @@
+import 'dart:convert';
+
 import 'package:flutter/foundation.dart';
+import 'package:http/http.dart' as http;
+
+import '../constants.dart';
 
 class Product with ChangeNotifier {
   final String id;
@@ -17,8 +22,30 @@ class Product with ChangeNotifier {
     this.isFavorite = false,
   });
 
-  void toggleFavoriteStatus() {
+  void _setFavValue(bool newValue) {
+    isFavorite = newValue;
+    notifyListeners();
+  }
+
+  Future<void> toggleFavoriteStatus() async {
+    final oldStatus = isFavorite;
     isFavorite = !isFavorite;
     notifyListeners();
+    Uri url = Uri.parse(
+      '$mainUrl/products/$id.json',
+    );
+    try {
+      final response = await http.patch(
+        url,
+        body: json.encode({
+          'isFavorite': isFavorite,
+        }),
+      );
+      if (response.statusCode >= 400) {
+        _setFavValue(oldStatus);
+      }
+    } catch (error) {
+      _setFavValue(oldStatus);
+    }
   }
 }
