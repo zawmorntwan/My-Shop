229686971b95db80f81a2830e4b3e0d5342f94c6
Zaw Min Tun <zawmintun.tp@gmail.com>
2022-11-27 19:06:43 +0630

    Update order_card.dart

--- lib/widgets/order_card.dart
+++ lib/widgets/order_card.dart
@@ -17,28 +17,31 @@ class _OrderCardState extends State<OrderCard> {
   var _expanded = false;
   @override
   Widget build(BuildContext context) {
-    return Card(
-      margin: const EdgeInsets.all(10),
-      child: Column(
-        children: [
-          ListTile(
-            title: Text('\$ ${widget.order.amount}'),
-            subtitle: Text(
-              DateFormat('dd/MM/yyyy hh:mm').format(widget.order.dateTime),
+    return AnimatedContainer(
+      duration: const Duration(milliseconds: 300),
+      height: _expanded ? min(widget.order.products.length * 25 + 110, 200) : 95,
+      child: Card(
+        margin: const EdgeInsets.all(10),
+        child: Column(
+          children: [
+            ListTile(
+              title: Text('\$ ${widget.order.amount}'),
+              subtitle: Text(
+                DateFormat('dd/MM/yyyy hh:mm').format(widget.order.dateTime),
+              ),
+              trailing: IconButton(
+                icon: Icon(_expanded ? Icons.expand_less : Icons.expand_more),
+                onPressed: () {
+                  setState(() {
+                    _expanded = !_expanded;
+                  });
+                },
+              ),
             ),
-            trailing: IconButton(
-              icon: Icon(_expanded ? Icons.expand_less : Icons.expand_more),
-              onPressed: () {
-                setState(() {
-                  _expanded = !_expanded;
-                });
-              },
-            ),
-          ),
-          if (_expanded)
-            Container(
+            AnimatedContainer(
+              duration: const Duration(milliseconds: 300),
               padding: const EdgeInsets.symmetric(horizontal: 15, vertical: 10),
-              height: min(widget.order.products.length * 20 + 15, 100),
+              height: _expanded ? min(widget.order.products.length * 25 + 15, 100) : 0,
               child: ListView(
                 physics: const BouncingScrollPhysics(),
                 children: widget.order.products
@@ -66,7 +69,8 @@ class _OrderCardState extends State<OrderCard> {
                     .toList(),
               ),
             )
-        ],
+          ],
+        ),
       ),
     );
   }
